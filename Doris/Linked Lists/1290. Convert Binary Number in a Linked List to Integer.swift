af8df4cd6e7b3598804a1527003352e7aeedd171
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-05-07 14:09:44 +0900

    ✨add: LTC_1290. Convert Binary Number in a Linked List to Integer

--- Doris/Linked Lists/1290. Convert Binary Number in a Linked List to Integer.swift
+++ Doris/Linked Lists/1290. Convert Binary Number in a Linked List to Integer.swift
@@ -8,9 +8,11 @@ class Solution {
             result += "\(node!.val)"
             node = node?.next
         }
+
         if let decimal = Int(result, radix: 2) {
             return decimal
         }
+        
         return 0
     }
 }
@@ -20,5 +22,7 @@ class Solution {
 ❗️ radix : 진수변환 파라미터
 ex) let number = Int("1010", radix: 2)
 위의 코드는 "1010"이 2진수임을 알려주고 이를 10진수로 변환한 값을 number에 저장하는 것
-❗️ ㅑㅜ
+❗️ Int(s, radix: 2) 에서 s에는 반드시 문자열이 와야함
+
+result의 초기값이 nil이므로 Int(result, radix:2)의 반환 타입이 (Int?)옵셔널 타입이므로 강제 언래핑 필요
 */

8f75c2e460b8ce974e199bb02976d5987620bfdc
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-05-08 03:16:39 +0900

    ✅MOD: ListNode class 추가

--- Doris/Linked Lists/206. Reverse Linked List.swift
+++ Doris/Linked Lists/206. Reverse Linked List.swift
@@ -1,4 +1,14 @@
 // Iterative Approach
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
         

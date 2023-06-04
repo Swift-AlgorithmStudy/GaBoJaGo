dd25382268725bd1454ab3bf830b632d892d553f
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-06-04 22:58:49 +0900

    Update 704. Binary Search.swift

--- Doris/Binary Search/704. Binary Search.swift
+++ Doris/Binary Search/704. Binary Search.swift
@@ -45,3 +45,23 @@ class Solution {
         return -1
     }
 }
+
+
+
+// 우니꺼
+
+
+// func search(_ nums: [Int], _ target: Int) -> Int {
+//     var start = 0, end = nums.count - 1
+//     while (조건) {
+//         let mid = start + (end - start) / 2
+//         if target == nums[mid] {
+//             return mid
+//         } else if target < nums[mid] {
+//             end = mid - 1
+//         } else {
+//             start = mid + 1
+//         }
+//     }
+//     return -1
+// }

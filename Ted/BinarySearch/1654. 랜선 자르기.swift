a1a1a51ad1eb3c1c076d34872eecdc7296f83b83
TaeHyung Kim (Ted Kim) <104834390+Taerogrammer@users.noreply.github.com>
2023-06-19 19:39:22 +0900

    Update 1654. 랜선 자르기.swift

--- Ted/BinarySearch/1654. 랜선 자르기.swift
+++ Ted/BinarySearch/1654. 랜선 자르기.swift
@@ -1,27 +1,44 @@
-/**
-1. nums를 정렬시킨다. (sortNums)
-2. query를 순회하면서 sortNums 있는 요소를 확인한다.
-3. 만약 query의 요소보다 sum(sortNums의 합) + sortNums가 크거나 같다면, 해당하는 값까지 포함시킬 수 있다는 것이기 때문에, 해당 값을 sum에 더해주고, cnt를 추가해준다.
-4. 만약 query를 한 바퀴 순회했다면 cnt을 result라는 배열에 추가해주고, sum과 cnt를 초기화해준다.
-5. nums까지 다 순회했다면 result를 반환한다.
+/***
+1. K와 N을 입력받는다.
+2. 배열에 값을 추가받는다.
+3. 최소 길이(start)와 최대 길이(end)를 설정한다. (사실상 left, right임)
+4. mid를 찾은 뒤, mid로 나눈 값들을 더하여 임시 선의 개수(temp)를 저장한다.
+5. 만약 temp가 N보다 크거나 같을 때(최소 개수 만족),
+5-1. 만약 result가 mid보다 작으면 result를 mid값으로 바꾼다.
+5-2. 만약 아니라면 오른쪽 부분을 탐색한다.
+6. 만약 temp가 작다면 왼쪽 부분을 탐색한다.
 */
 
-class Solution {
-    func answerQueries(_ nums: [Int], _ queries: [Int]) -> [Int] {
-        let n = nums.count, m = queries.count, sortNums = nums.sorted()
-        var sum = 0, cnt = 0, result = [Int]()
-        
-        for i in 0..<m {
-            for j in 0..<n {
-                if sum + sortNums[j] <= queries[i] {
-                    sum += sortNums[j]
-                    cnt += 1
-                }
-            }
-            result.append(cnt)
-            sum = 0
-            cnt = 0
-        }
-        return result
-    }
+let input = readLine()!.split(separator: " ").map({ Int($0)!})
+let K = input[0]
+let N = input[1]
+var result = 0
+var lines = [Int]()
+
+for _ in 0..<K {
+    lines.append(Int(readLine()!)!)
 }
+
+var start = 1 //최소 길이
+var end = lines.max()!  //최대 길이
+
+while start <= end {
+    let mid = (start + end) / 2
+    var temp = 0
+    
+    for line in lines {
+        temp += line / mid
+    }
+    
+    if temp >= N {
+        if result < mid {
+            result = mid
+        }
+        start = mid + 1
+    } else {
+        end = mid - 1
+    }
+    
+}
+
+print(result)
